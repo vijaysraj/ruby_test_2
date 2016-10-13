50a03aee1d75797c75469160d3ec8a662c5a1eea
Mr. Sujay HV <svijay@qwinix.io>
2016-10-13 17:12:47 +0530

    The programs done after noon(final commit)

--- 1.rb
+++ 1.rb
@@ -1,7 +1,7 @@
 puts "Say something to Qwinix!"
 something = gets.chomp
-while something != "BYE"
-    if something == something.pcase
+while something == "BYE"
+    if something == something.upcase
       puts "NO, NOT SINCE 20" + (rand(11..16)).to_s + "!"
     else
       puts "HUH? SPEAK UP!"
